4a2ade759942a67eb8b5b471ffce225f65e53422
Sonny Sangha <sonny.sangha@gmail.com>
2019-01-14 11:54:09 +0000

    cleanup

--- src/sagas/sampleSaga.ts
+++ src/sagas/sampleSaga.ts
@@ -1,9 +1,6 @@
-import { takeEvery, put } from 'redux-saga/effects';
-import { types as sampleTypes, loadImagesSuccess } from '../actions/sampleActions';
+import { takeEvery } from 'redux-saga/effects';
+import { types as sampleTypes } from '../actions/sampleActions';
 import { SagaIterator } from 'redux-saga';
-
-const vision = require('node-cloud-vision-api')
-vision.init({ auth: 'AIzaSyBaVgdjfBjX-S5EfKhvdZj_DKhju9mbPNI' })
 
 function* loadImages(): SagaIterator {
     console.log('abc');
